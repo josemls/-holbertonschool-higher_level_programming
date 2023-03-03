1af29130e33a027959637b233935eb57a956dcb9
josemls <jose_9513@hotmail.com>
2023-03-03 01:35:01 -0400

    base

---
+++ python-almost_a_cricle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class base"""
+
+import json
+import csv
+import os.path
+
+
+class Base:
+        """ class called Base"""
+        __nb_objects = 0
+
+        def __init__(self, id=None):
+                """ initializer method"""
+                if id is not None:
+                        self.id = id
+                else:
+                        Base.__nb_objects += 1
+                        self.id = self.__nb_objects
