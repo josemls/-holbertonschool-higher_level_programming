9be20586a4105cf1dfc96e15b121aaa1a9d72880
josemls <jose_9513@hotmail.com>
2023-03-04 11:37:33 -0400

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,3 +23,20 @@ class Base:
                 if list_dictionaries is None:
                         return "[]"
                 return json.dumps(list_dictionaries)
+
+        @classmethod
+        def save_to_file(cls, list_objs):
+                """Writes the JSON string representation """
+                filename = "{}.json".format(cls.__name__)
+                list_dic = []
+
+                if list_objs is None:
+                        pass
+                else:
+                        for i in range(len(list_objs)):
+                                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as file:
+            file.write(lists)
