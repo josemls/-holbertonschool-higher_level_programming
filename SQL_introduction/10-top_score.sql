4d7922548f569632a37b58e0cd142fc7cc364dcf
josemls <jose_9513@hotmail.com>
2023-04-26 12:36:59 -0400

    10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,2 @@
--- lists all records of 'second_table' of database 
---in descending order 
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+--script that lists all records of the table second_table of the database
+SELECT score, name FROM second_table ORDER BY score DESC;
