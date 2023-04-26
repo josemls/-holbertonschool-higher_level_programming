2e4372d22b1266f62e0c4979bd4ea0f267984aad
josemls <jose_9513@hotmail.com>
2023-04-26 13:12:53 -0400

    utf8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -6,10 +6,10 @@ ALTER DATABASE
 ALTER TABLE
 	hbtn_0c_0.first_table
       CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+      COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE
 	hbtn_0c_0.first_table
 	MODIFY name
 	VARCHAR(256)
 	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+	COLLATE = utf8mb4_unicode_ci;
