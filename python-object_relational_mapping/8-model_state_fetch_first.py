282e9cba1744edce911151116ca21b2f8c2c56b0
josemls <jose_9513@hotmail.com>
2023-05-13 13:34:53 -0400

    model

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Lists all State objects from the database"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    # state = sys.argv[4]
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    url = url.format(user, password, db_name)
+
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id)
+    state = query.first()
+
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    
