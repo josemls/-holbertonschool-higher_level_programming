97089ebd1a2bb1aab478d6eefce5d67391c50807
josemls <jose_9513@hotmail.com>
2023-05-13 14:18:17 -0400

    model state get

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""script that prints the State object with the name passed as argument
- from the database hbtn_0e_6_usa"""
+"""script that prints the State object with the name passed as arguments"""
 
 from sqlalchemy import create_engine
 from sys import argv
