447fd07da05949f426363f870b56e5cbe3f68da9
josemls <jose_9513@hotmail.com>
2023-07-08 13:52:27 -0400

    fix

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-// reverses a list
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  const reversedList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+
+  return reversedList;
 };
